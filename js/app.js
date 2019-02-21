35fc615ba8af45157d771663840b5b5cd1fb94f4
Chandra Kolluri <kolluc@amazon.com>
2019-02-20 20:09:37 -0800

    January born logic

--- js/app.js
+++ js/app.js
@@ -5,11 +5,34 @@ alert('Hi ' + userName + '. Welcome!');
 alert('Lets start the game. Answer few questions with simple Yes or No answers');
 alert('I will guess your Sun sign :) ')
 
-var partOfTheYear = prompt("Are you born in first 6 months of the year ?");
-if (partOfTheYear.toLocaleUpperCase === "YES" || partOfTheYear.toLocaleUpperCase === "Y")
-{
-console.log('Great! You answered' + partOfTheYear + '. So you born between January and June.');
-alert("Great! You answered" + partOfTheYear + " So you born between January and June.");
+var partOfTheYear = prompt("Are you born in first 6 months of the year?");
+if (partOfTheYear.toLocaleUpperCase === "YES" || partOfTheYear.toLocaleUpperCase === "Y"){
+    console.log('Great! You answered' + partOfTheYear + '. So you born between January and June.');
+    alert("Great! You answer is " + partOfTheYear + ", so you born between January and June.");
+
+    var monthPrefixJ = prompt("Does your birth month Starts with letter J?");
+
+    if (monthPrefixJ.toLocaleUpperCase === "YES" || monthPrefixJ.toLocaleUpperCase === "Y") {
+    console.log('You confirmed, your birth month starts with letter J.');
+    var monthSuffixY = prompt("Does your birth month ends with letter Y?");
+    if (monthSuffixY.toLocaleUpperCase === "YES" || monthSuffixY.toLocaleUpperCase === "Y"){
+        console.log('You confirmed, your birth month ends with letter Y.');
+        var dateRangeJan = prompt("Does your birth date falls between 1 and 19?");
+        if (monthSuffixY.toLocaleUpperCase === "YES" || monthSuffixY.toLocaleUpperCase === "Y"){
+            Console.log('Your birth date falls between 1 and 19')
+            alert('Thanks for your responses ' + userName + '.Your birth month is January and you Sunsign is Aquarius')
+        } else {
+            Console.log('Your birth date falls between 20 and 31')
+            alert('Thanks for your responses ' + userName + '. You are a Aquarius')
+        }
+    }
+   
+    }
+
+
+
+
+
 } else {
     console.log('Great! You answered' + partOfTheYear + " So you born between July and December.");
     alert("Great! You answered" + partOfTheYear + " So you born between July and December.");
